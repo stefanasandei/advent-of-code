2a541c997e5c21b1b2bb32253009bdf500e9a415
Stefan Asandei <asandei.stefanel@gmail.com>
2024-12-04 17:12:56 +0200

    day 3

--- 2024/day-03/part1.c
+++ 2024/day-03/part1.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,15 +11,50 @@ void err_n_die(bool cond, char *msg) {
   exit(EXIT_FAILURE);
 }
 
-char line[256];
+char line[4096];
 int ans;
 
 void solve(void) {
-  ans++;
+  const size_t len = strlen("mul(");
+
+  for (size_t i = 0; i < strlen(line); i++) {
+    if (strncmp(line + i, "mul(", len) != 0) {
+      continue;
+    }
+    i += len;
+
+    if (!isdigit(line[i]))
+      continue;
+
+    int a = 0, b = 0;
+
+    while (isdigit(line[i])) {
+      a = a * 10 + (line[i] - '0');
+      i++;
+    }
+
+    if (line[i] != ',')
+      continue;
+    i++;
+
+    if (!isdigit(line[i]))
+      continue;
+
+    while (isdigit(line[i])) {
+      b = b * 10 + (line[i] - '0');
+      i++;
+    }
+
+    if (line[i] != ')')
+      continue;
+
+    // printf("%d, %d\n", a, b);
+    ans += a * b;
+  }
 }
 
 int main(void) {
-  FILE *fp = fopen("./day-03/example1.txt", "r");
+  FILE *fp = fopen("./day-03/input.txt", "r");
   err_n_die(fp == NULL, "Couldn't find the input file.");
 
   while (fgets(line, sizeof(line), fp) != NULL) {
