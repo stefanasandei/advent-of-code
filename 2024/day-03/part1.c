7d05c935a149d06603342c6188f285a72014acef
Asandei Stefan <asandei.stefanel@gmail.com>
2024-12-04 10:54:01 +0200

    prepare for days 3 & 4

---
+++ 2024/day-03/part1.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void err_n_die(bool cond, char *msg) {
+  if (!cond)
+    return;
+  fprintf(stderr, "%s\n", msg);
+  exit(EXIT_FAILURE);
+}
+
+char line[256];
+int ans;
+
+void solve(void) {
+  ans++;
+}
+
+int main(void) {
+  FILE *fp = fopen("./day-03/example1.txt", "r");
+  err_n_die(fp == NULL, "Couldn't find the input file.");
+
+  while (fgets(line, sizeof(line), fp) != NULL) {
+    solve();
+  }
+
+  printf("%d\n", ans);
+
+  fclose(fp);
+  return 0;
+}
