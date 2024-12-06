24773b8146916a87f2a0bf59cc5f3ddf5d2c02c8
Asandei Stefan <asandei.stefanel@gmail.com>
2024-12-06 11:03:50 +0200

    part of day 4

--- 2024/day-04/part1.c
+++ 2024/day-04/part1.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LEN(x) sizeof(x) / sizeof(x[0])
+
 void err_n_die(bool cond, char *msg) {
   if (!cond)
     return;
@@ -10,21 +12,51 @@ void err_n_die(bool cond, char *msg) {
   exit(EXIT_FAILURE);
 }
 
-char line[256];
-int ans;
+const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+char line[256], map[256][256];
+int ans,w, h;
+
+bool ok(int x, int y) {
+    return x >= 0 && x < w && y >= 0 && y < h;
+}
 
 void solve(void) {
-  ans++;
+    const char pattern[] = "XMAS";
+    w = strlen(map[0]);
+
+    for(int i=0; i<h; i++) {
+        for(int j=0; j<w; j++) {
+            for(size_t k = 0; k < LEN(dx); k++) {
+                bool found = true;
+
+                for(size_t l=0; l < strlen(pattern) && found; l++) {
+                    int x = i + dx[k] * l;
+                    int y = j + dy[k] * l;
+
+                    if(!ok(x, y)) {
+                        found = false;
+                    } else {
+                        found = pattern[l] == map[x][y];
+                    }
+                }
+
+                ans += found;
+            }
+        }
+    }
 }
 
 int main(void) {
-  FILE *fp = fopen("./day-04/example1.txt", "r");
+  FILE *fp = fopen("./day-04/input.txt", "r");
   err_n_die(fp == NULL, "Couldn't find the input file.");
 
   while (fgets(line, sizeof(line), fp) != NULL) {
-    solve();
+      strcpy(map[h++], line);
   }
 
+  solve();
   printf("%d\n", ans);
 
   fclose(fp);
--- 2024/day-04/part2.c
+++ 2024/day-04/part2.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LEN(x) sizeof(x) / sizeof(x[0])
+// not working yet
 void err_n_die(bool cond, char *msg) {
   if (!cond)
     return;
@@ -10,23 +12,57 @@ void err_n_die(bool cond, char *msg) {
   exit(EXIT_FAILURE);
 }
 
-char line[256];
-int ans;
+const char pattern[] = "MAS";
+const int dx[] = {1, -1};
+const int dy[] = {1, -1};
+
+char line[256], map[256][256];
+int ans,w, h;
+
+bool ok(int x, int y) {
+    return x >= 0 && x < h && y >= 0 && y < w;
+}
+
+bool check_diag(int i, int j, bool up, bool backwards) {
+    for (int l = 0; l < (int)strlen(pattern); l++) {
+        int x = i + dx[up ? 1 : 0] * (backwards ? strlen(pattern) - 1 - l : l);
+        int y = j + dy[up ? 1 : 0] * (backwards ? strlen(pattern) - 1 - l : l);
+
+        if (!ok(x, y) || map[x][y] != pattern[l]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool check_diag_pair(int i, int j) {
+    bool down = check_diag(i, j, false, false) || check_diag(i, j, false, true);
+    bool up = check_diag(i+2, j+2, true, false) || check_diag(i+2, j+2, true, true);
+    return up && down;
+}
 
 void solve(void) {
-  ans++;
+    w = strlen(map[0]);
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w - 2; j++) {
+            ans += check_diag_pair(i, j);
+        }
+    }
 }
 
 int main(void) {
-  FILE *fp = fopen("./day-04/example2.txt", "r");
-  err_n_die(fp == NULL, "Couldn't find the input file.");
+    FILE *fp = fopen("./day-04/input.txt", "r");
+    err_n_die(fp == NULL, "Couldn't find the input file.");
 
-  while (fgets(line, sizeof(line), fp) != NULL) {
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        line[strcspn(line, "\n")] = 0;
+        strcpy(map[h++], line);
+    }
+
     solve();
-  }
+    printf("%d\n", ans);
 
-  printf("%d\n", ans);
-
-  fclose(fp);
-  return 0;
+    fclose(fp);
+    return 0;
 }
